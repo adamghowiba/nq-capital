ca47fc779e500b2358ef65c659d2849277cde29f
Adam <adamware99@hotmail.com>
2024-02-06 19:16:12 +0400

    chore: migration

--- apps/api/src/app/app.module.ts
+++ apps/api/src/app/app.module.ts
@@ -1,10 +1,21 @@
 import { Module } from '@nestjs/common';
-
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import {
+  ApolloDriver,
+  ApolloDriverConfig,
+} from '@nestjs/apollo';
+import { GraphQLModule } from '@nestjs/graphql';
 
 @Module({
-  imports: [],
+  imports: [
+    GraphQLModule.forRoot<ApolloDriverConfig>({
+      driver: ApolloDriver,
+      playground: true,
+      introspection: true,
+      autoSchemaFile: 'schema.gql',
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
