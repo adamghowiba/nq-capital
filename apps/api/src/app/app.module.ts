d870f7d9db99ab104525c54efc7434ef9e83b407
Adam <adamware99@hotmail.com>
2024-02-06 19:19:10 +0400

    feat: users module

--- apps/api/src/app/app.module.ts
+++ apps/api/src/app/app.module.ts
@@ -1,14 +1,15 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import {
-  ApolloDriver,
-  ApolloDriverConfig,
-} from '@nestjs/apollo';
+import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
 import { GraphQLModule } from '@nestjs/graphql';
+import { UsersModule } from '../modules/users/users.module';
+
+const APP_MODULES = [UsersModule];
 
 @Module({
   imports: [
+    ...APP_MODULES,
     GraphQLModule.forRoot<ApolloDriverConfig>({
       driver: ApolloDriver,
       playground: true,
