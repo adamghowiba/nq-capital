f641529e085559ea160fc4fd87795d3fa3e3585a
Tchakoumi <lorraintchakoumi@gmail.com>
2024-02-28 12:29:06 +0100

    feat(app): #3 setup dashboard layout in index file (wip)

--- apps/investors-portal-web/pages/index.tsx
+++ apps/investors-portal-web/pages/index.tsx
@@ -1,12 +1,15 @@
 import { Box, Button } from '@mui/material';
+import DashboardLayout from '../components/(dashboard)/layout';
 
 export function Index() {
   return (
-    <Box>
-      <Button variant="contained" color="primary">
-        Testing
-      </Button>
-    </Box>
+    <DashboardLayout>
+      <Box>
+        <Button variant="contained" color="primary">
+          Testing
+        </Button>
+      </Box>
+    </DashboardLayout>
   );
 }
 
