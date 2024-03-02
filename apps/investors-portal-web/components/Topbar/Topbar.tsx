8394cc98337836707ab6d925acd4cff15113f7bc
Tchakoumi <lorraintchakoumi@gmail.com>
2024-03-02 07:21:00 +0100

    feat(app): #3 setup topBar with  all elements (WIP)

--- apps/investors-portal-web/components/Topbar/Topbar.tsx
+++ apps/investors-portal-web/components/Topbar/Topbar.tsx
@@ -1,32 +1,52 @@
+import notification from '@iconify/icons-fluent/alert-24-filled';
+import upDown from '@iconify/icons-fluent/chevron-up-down-24-filled';
+import person from '@iconify/icons-fluent/person-24-filled';
+import questionCircle from '@iconify/icons-fluent/question-circle-24-filled';
+import settings from '@iconify/icons-fluent/settings-24-filled';
+import { Icon } from '@iconify/react';
+import { Avatar, Divider, IconButton, Tooltip } from '@mui/material';
 import Box from '@mui/material/Box';
-import Grid from '@mui/material/Grid';
-import IconButton from '@mui/material/IconButton';
-import InputAdornment from '@mui/material/InputAdornment';
-import Stack from '@mui/material/Stack';
-import TextField from '@mui/material/TextField';
 import Typography from '@mui/material/Typography';
-import { FC, useEffect, useState } from 'react';
-import { useRouter } from 'next/router';
-import Tooltip from '@mui/material/Tooltip';
 import DContainer from '../DContainer/DContainer';
+import OneIcon from '../OneIcon/OneIcon';
 import TopbarSearch from './TopbarSearch';
 
-export interface TopbarProps {}
-
-const Topbar: FC<TopbarProps> = ({ ...props }) => {
+function Topbar() {
   return (
     <>
       <Box width="100%" bgcolor="#F9F9F9" height="56px">
-        <DContainer height="100%" width="100%">
-          <Grid container width="100%" height="100%" alignContent="center">
-            <Grid item xs alignSelf="center">
-              <Stack
+        <DContainer
+          height="100%"
+          width="100%"
+          sx={{ display: 'grid', alignItems: 'center' }}
+        >
+          <Box
+            sx={{
+              display: 'grid',
+              gridTemplateColumns: 'auto 1fr auto',
+              justifyItems: 'center',
+              alignItems: 'center',
+            }}
+          >
+            <Box
+              sx={{
+                display: 'grid',
+                gridAutoFlow: 'column',
+                alignItems: 'center',
+                columnGap: 3,
+                justifyItems: 'center',
+              }}
+            >
+              <Box
                 bgcolor="black"
                 borderRadius="8px"
                 width="36px"
                 height="32px"
-                alignItems="center"
-                justifyContent="center"
+                sx={{
+                  display: 'grid',
+                  alignItems: 'center',
+                  justifyContent: 'center',
+                }}
               >
                 <Typography
                   color="white"
@@ -34,46 +54,56 @@ const Topbar: FC<TopbarProps> = ({ ...props }) => {
                   fontWeight="500"
                   lineHeight="1"
                 >
-                  AO
+                  NQ
                 </Typography>
-              </Stack>
-            </Grid>
-
-            <Grid item xs>
-              <TopbarSearch />
-            </Grid>
-
-            <Grid
-              item
-              xs
-              display="flex"
-              justifyContent="end"
-              alignItems="center"
-              color="#8D8D8D"
+              </Box>
+              <Divider orientation="vertical" sx={{ height: '60%' }} />
+              <Box
+                sx={{
+                  display: 'grid',
+                  columnGap: '9px',
+                  gridAutoFlow: 'column',
+                  alignItems: 'center',
+                  justifyContent: 'start',
+                }}
+              >
+                <Avatar
+                  sx={{
+                    bgcolor: '#EBEBEB',
+                    fontWeight: '500',
+                    fontSize: '12px',
+                    color: '#8D8D8D',
+                  }}
+                >
+                  JS
+                </Avatar>
+                <Typography>Jane Smith</Typography>
+                <Tooltip arrow title="Swap user">
+                  <IconButton size="small">
+                    <Icon icon={upDown} fontSize="16px" />
+                  </IconButton>
+                </Tooltip>
+              </Box>
+            </Box>
+            <TopbarSearch />
+            <Box
+              sx={{
+                display: 'grid',
+                alignItems: 'center',
+                gridAutoFlow: 'column',
+                columnGap: 2,
+              }}
             >
-              {/* <IconButton sx={{ color: 'inherit' }}>
-                <HelpIcon sx={{ width: ICON_SIZE }} />
-              </IconButton> */}
-
-              {/* <IconButton sx={{ color: 'inherit' }}>
-                <NotificationsIcon sx={{ width: ICON_SIZE }} />
-              </IconButton> */}
-
-              {/* <IconButton sx={{ color: 'inherit' }}>
-                <SettingsIcon sx={{ width: ICON_SIZE }} />
-              </IconButton> */}
-
-              {/* <Tooltip title={user?.user?.email}>
-                <IconButton sx={{ color: 'inherit' }}>
-                  <Person sx={{ width: ICON_SIZE }} />
-                </IconButton>
-              </Tooltip> */}
-            </Grid>
-          </Grid>
+              <OneIcon title="Help" icon={questionCircle} />
+              <OneIcon title="Notifications" icon={notification} />
+              <OneIcon title="Settings" icon={settings} />
+              <OneIcon title="Profile" icon={person} />
+            </Box>
+          </Box>
         </DContainer>
       </Box>
     </>
   );
-};
+}
 
 export default Topbar;
