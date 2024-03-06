235157c79f2b0acbff2be354dfa82f2a444d4b57
Lorrain Tchakoumi <lorraintchakoumi@gmail.com>
2024-03-06 06:27:08 +0100

    feat(app): #7 added financial information component for onboarding

---
+++ apps/investors-portal-web/components/onboarding/financialInformation.tsx
@@ -0,0 +1,118 @@
+import add from '@iconify/icons-fluent/add-24-filled';
+import { Box, Button, CircularProgress, Typography } from '@mui/material';
+import { useState } from 'react';
+import OneIcon from '../utils/OneIcon';
+import BankCard from './bankCard';
+import NewBankDialog, { NewBankData } from './newBankDialog';
+import StepHeader from './stepHeader';
+
+export interface FinancialInformationProps {
+  data: NewBankData[];
+  isSubmitting: boolean;
+  onNext: (banks: NewBankData[]) => void;
+  onBack: (banks: NewBankData[]) => void;
+}
+export default function FinancialInformation({
+  data,
+  onNext,
+  onBack,
+  isSubmitting,
+}: FinancialInformationProps) {
+  const [isNewBankDialogOpen, setIsNewBankDialogOpen] =
+    useState<boolean>(false);
+
+  const [bankAccounts, setBankAccounts] = useState<NewBankData[]>(data);
+
+  return (
+    <>
+      <NewBankDialog
+        isDialogOpen={isNewBankDialogOpen}
+        closeDialog={() => setIsNewBankDialogOpen(false)}
+        handleAddBank={(newBankAccount) =>
+          setBankAccounts([...bankAccounts, newBankAccount])
+        }
+      />
+      <Box width={500} sx={{ display: 'grid', rowGap: 5 }}>
+        <StepHeader
+          subtitle="Share details about your invested funds and preferred bank accounts."
+          title="Financial Information"
+        />
+        <Box sx={{ display: 'grid', rowGap: 8 }}>
+          <Box sx={{ display: 'grid', rowGap: 2 }}>
+            <Box
+              sx={{
+                display: 'grid',
+                gridTemplateColumns: '1fr auto',
+                alignItems: 'center',
+              }}
+            >
+              <Typography>Bank Details</Typography>
+              {bankAccounts.length > 0 && (
+                <OneIcon
+                  icon={add}
+                  title="Add new bank"
+                  fontSize={16}
+                  iconColor="#808080"
+                  onClick={() => setIsNewBankDialogOpen(true)}
+                  disabled={isSubmitting}
+                />
+              )}
+            </Box>
+            {bankAccounts.length === 0 ? (
+              <Box
+                component={Button}
+                fullWidth
+                size="large"
+                onClick={() => setIsNewBankDialogOpen(true)}
+                sx={{
+                  border: '2px dotted #E4E4E4',
+                  color: '#BBBBBB',
+                  '&.MuiButtonBase-root': {
+                    paddingTop: '22px',
+                    paddingBottom: '22px',
+                  },
+                }}
+              >
+                Add Bank Detail
+              </Box>
+            ) : (
+              bankAccounts.map((bank, index) => (
+                <BankCard key={index} bank={bank} disabled={isSubmitting} />
+              ))
+            )}
+          </Box>
+          <Box
+            sx={{
+              display: 'grid',
+              gridTemplateColumns: 'auto 1fr',
+              columnGap: 1,
+              alignItems: 'center',
+            }}
+          >
+            <Button
+              color="secondary"
+              variant="contained"
+              disabled={isSubmitting}
+              onClick={() => onBack(bankAccounts)}
+            >
+              Back
+            </Button>
+            <Button
+              color="primary"
+              variant="contained"
+              disabled={isSubmitting}
+              onClick={() => onNext(bankAccounts)}
+              endIcon={
+                isSubmitting && (
+                  <CircularProgress color="primary" thickness={5} size={14} />
+                )
+              }
+            >
+              {isSubmitting ? 'Submitting' : 'Next'}
+            </Button>
+          </Box>
+        </Box>
+      </Box>
+    </>
+  );
+}
