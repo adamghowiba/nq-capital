81d254e747d88fa7443952df8e593aa205b9728b
Lorrain Tchakoumi <lorraintchakoumi@gmail.com>
2024-03-08 07:53:44 +0100

    feat(app): #11 (WIP) building the billing screen

--- apps/investors-portal-web/components/settings/index.tsx
+++ apps/investors-portal-web/components/settings/index.tsx
@@ -1,5 +1,7 @@
-import { Dialog } from '@mui/material';
-import { useState } from 'react';
+import { Box, Dialog } from '@mui/material';
+import { useEffect, useState } from 'react';
+import ListBanks from '../onboarding/listBanks';
+import { NewBankData } from '../onboarding/newBankDialog';
 import DialogTransition from '../utils/DialogTransition';
 import SettingsHeader from './header';
 import SettingsTopbar from './topbar';
@@ -13,6 +15,27 @@ export default function SettingsDialog({
   isDialogOpen,
 }: SettingsDialogProps) {
   const [activeTab, setActiveTab] = useState<number>(1);
+  const [isLoadingBanks, setIsLoadingBanks] = useState<boolean>(false);
+  const [bankAccounts, setBankAccounts] = useState<NewBankData[]>([]);
+  const [isSubmitting, setIsSubmitting] = useState<boolean>(false);
+
+  useEffect(() => {
+    setIsLoadingBanks(true);
+    //TODO: CALL API HERE TO LOAD BANKS
+    setTimeout(() => {
+      setIsLoadingBanks(false);
+      setBankAccounts([]);
+    }, 3000);
+  }, []);
+
+  function changeDefaultBank(bank: NewBankData) {
+    setIsSubmitting(true);
+    //TODO: CALL API HER TO CHANGE DEFAULT BANK AND MUTATE AFTERWARDS.
+    setTimeout(() => {
+      setIsSubmitting(false);
+    }, 3000);
+  }
+
   return (
     <Dialog
       fullScreen
@@ -25,6 +48,15 @@ export default function SettingsDialog({
         activeTab={activeTab}
         handleTabChange={(tabNumber) => setActiveTab(tabNumber)}
       />
+      <Box sx={{ paddingTop: 5, display: 'grid', justifyItems: 'center' }}>
+        <Box sx={{ width: '604px' }}>
+          <ListBanks
+            bankAccounts={bankAccounts}
+            isSubmitting={isSubmitting}
+            handleChangeDefault={changeDefaultBank}
+          />
+        </Box>
+      </Box>
     </Dialog>
   );
 }
