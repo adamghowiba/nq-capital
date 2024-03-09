7ff7fc3e4e57c3ed50ad3b5662226ccd6e7c59b9
Lorrain Tchakoumi <lorraintchakoumi@gmail.com>
2024-03-09 15:06:25 +0100

    feat(app): #11 added delete account section to general in settings

--- apps/investors-portal-web/components/settings/general.tsx
+++ apps/investors-portal-web/components/settings/general.tsx
@@ -3,6 +3,8 @@ import { useFormik } from 'formik';
 import { useEffect, useState } from 'react';
 import * as Yup from 'yup';
 import { OneTextField } from '../utils/OneTextField';
+import { ConfirmDialog } from '../utils/ConfirmDialog';
+import { useRouter } from 'next/router';
 
 interface IUserData {
   email: string;
@@ -11,6 +13,7 @@ interface IUserData {
 }
 
 export default function General() {
+  const { push } = useRouter();
   const [isLoadingUserData, setIsLoadingUserData] = useState<boolean>(false);
   const [userData, setUserData] = useState<IUserData>({
     email: '',
@@ -64,61 +67,114 @@ export default function General() {
     },
   });
 
+  const [isConfirmDialogOpen, setIsConfirmDialogOpen] =
+    useState<boolean>(false);
+
+  const [isDeletingAccount, setIsDeletingAccount] = useState<boolean>(false);
+  function deleteAccount() {
+    //TODO: CALL API HERE TO DELETE ACCOUNT
+    setIsDeletingAccount(true);
+    setTimeout(() => {
+      setIsDeletingAccount(false);
+      setIsConfirmDialogOpen(false);
+      //TODO: navigate user to signup or landing page after successful account deletion.
+      push('/signup');
+    }, 3000);
+  }
+
   return (
-    <Box
-      component="form"
-      onSubmit={formik.handleSubmit}
-      sx={{ display: 'grid', rowGap: 5 }}
-    >
-      <Divider />
-      <OneTextField
-        OneLabel="Email"
-        placeholder="Enter your email"
-        required
-        type="email"
-        error={formik.touched.email && Boolean(formik.errors.email)}
-        helperText={formik.touched.email && formik.errors.email}
-        {...formik.getFieldProps('email')}
-        disabled={isSubmitting || isLoadingUserData}
+    <>
+      <ConfirmDialog
+        closeDialog={() => setIsConfirmDialogOpen(false)}
+        dialogMessage="Are you sure you want to delete your account? Note that this action is irreversible!"
+        isDialogOpen={isConfirmDialogOpen}
+        confirm={deleteAccount}
+        closeOnConfirm
+        dialogTitle="Delete Account"
+        danger
+        isSubmitting={isDeletingAccount}
       />
-      <OneTextField
-        OneLabel="First name"
-        placeholder="Enter your first name"
-        required
-        error={formik.touched.first_name && Boolean(formik.errors.first_name)}
-        helperText={formik.touched.first_name && formik.errors.first_name}
-        {...formik.getFieldProps('first_name')}
-        disabled={isSubmitting || isLoadingUserData}
-      />
-      <OneTextField
-        OneLabel="Last name"
-        placeholder="Enter your last name"
-        required
-        error={formik.touched.last_name && Boolean(formik.errors.last_name)}
-        helperText={formik.touched.last_name && formik.errors.last_name}
-        {...formik.getFieldProps('last_name')}
-        disabled={isSubmitting || isLoadingUserData}
-      />
-      {submitError && !isSubmitting && !isLoadingUserData && (
-        <Typography color="error">{submitError}</Typography>
-      )}
-      <Button
-        type="submit"
-        color="primary"
-        variant="contained"
-        sx={{
-          justifySelf: 'start',
-        }}
-        disabled={
-          isSubmitting ||
-          (formik.values.email === userData.email &&
-            formik.values.first_name === userData.first_name &&
-            formik.values.last_name === userData.last_name)
-        }
-      >
-        Save Changes
-      </Button>
-      <Divider />
-    </Box>
+      <Box>
+        <Box
+          component="form"
+          onSubmit={formik.handleSubmit}
+          sx={{ display: 'grid', rowGap: 5 }}
+        >
+          <Divider />
+          <OneTextField
+            OneLabel="Email"
+            placeholder="Enter your email"
+            required
+            type="email"
+            error={formik.touched.email && Boolean(formik.errors.email)}
+            helperText={formik.touched.email && formik.errors.email}
+            {...formik.getFieldProps('email')}
+            disabled={isSubmitting || isLoadingUserData}
+          />
+          <OneTextField
+            OneLabel="First name"
+            placeholder="Enter your first name"
+            required
+            error={
+              formik.touched.first_name && Boolean(formik.errors.first_name)
+            }
+            helperText={formik.touched.first_name && formik.errors.first_name}
+            {...formik.getFieldProps('first_name')}
+            disabled={isSubmitting || isLoadingUserData}
+          />
+          <OneTextField
+            OneLabel="Last name"
+            placeholder="Enter your last name"
+            required
+            error={formik.touched.last_name && Boolean(formik.errors.last_name)}
+            helperText={formik.touched.last_name && formik.errors.last_name}
+            {...formik.getFieldProps('last_name')}
+            disabled={isSubmitting || isLoadingUserData}
+          />
+          {submitError && !isSubmitting && !isLoadingUserData && (
+            <Typography color="error">{submitError}</Typography>
+          )}
+          <Button
+            type="submit"
+            color="primary"
+            variant="contained"
+            sx={{
+              justifySelf: 'start',
+            }}
+            disabled={
+              isSubmitting ||
+              (formik.values.email === userData.email &&
+                formik.values.first_name === userData.first_name &&
+                formik.values.last_name === userData.last_name)
+            }
+          >
+            Save Changes
+          </Button>
+          <Divider />
+        </Box>
+        <Box
+          sx={{
+            pt: 5,
+            display: 'grid',
+            rowGap: 2,
+            justifyItems: 'start',
+          }}
+        >
+          <Box>
+            <Typography>Delete Account</Typography>
+            <Typography color="#BBBBBB">
+              This action could not be undone
+            </Typography>
+          </Box>
+          <Button
+            color="error"
+            variant="contained"
+            onClick={() => setIsConfirmDialogOpen(true)}
+          >
+            Delete
+          </Button>
+        </Box>
+      </Box>
+    </>
   );
 }
