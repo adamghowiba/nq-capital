e88733add03c7ebb09c6595b3504c393b22cc134
Lorrain Tchakoumi <lorraintchakoumi@gmail.com>
2024-03-09 14:56:32 +0100

    feat(app): #11 added a confirmDialog to confirm actions

---
+++ apps/investors-portal-web/components/utils/ConfirmDialog.tsx
@@ -0,0 +1,99 @@
+import {
+  Button,
+  CircularProgress,
+  Dialog,
+  DialogActions,
+  DialogContent,
+  DialogContentText,
+  DialogTitle,
+  Typography,
+} from '@mui/material';
+import DialogTransition from './DialogTransition';
+
+export function ConfirmDialog({
+  isDialogOpen,
+  closeDialog,
+  confirm,
+  dialogMessage,
+  dialogTitle = 'Delete',
+  confirmButton = 'Delete',
+  danger = false,
+  closeOnConfirm = false,
+  isSubmitting = false,
+}: {
+  isDialogOpen: boolean;
+  closeDialog: () => void;
+  confirm: () => void;
+  dialogMessage: string;
+  dialogTitle?: string;
+  confirmButton?: string;
+  danger?: boolean;
+  closeOnConfirm?: boolean;
+  isSubmitting?: boolean;
+}) {
+  return (
+    <Dialog
+      open={isDialogOpen}
+      TransitionComponent={DialogTransition}
+      keepMounted
+      onClose={() => (isSubmitting ? null : closeDialog())}
+      sx={{
+        '& .MuiPaper-root': {
+          padding: { laptop: '20px', mobile: 0 },
+          borderRadius: '12px',
+        },
+      }}
+    >
+      <DialogTitle>
+        <Typography
+          variant="h3"
+          textAlign="start"
+          color={danger ? 'error' : 'initial'}
+        >
+          {dialogTitle}
+        </Typography>
+      </DialogTitle>
+      <DialogContent>
+        <DialogContentText sx={{ textAlign: 'center' }}>
+          <Typography>{dialogMessage}</Typography>
+        </DialogContentText>
+      </DialogContent>
+      <DialogActions
+        sx={{
+          justifyContent: 'center',
+          display: 'grid',
+          gridAutoFlow: 'column',
+          columnGap: '20px',
+        }}
+      >
+        <Button
+          color="secondary"
+          variant="contained"
+          onClick={() => (isSubmitting ? null : closeDialog())}
+          disabled={isSubmitting}
+        >
+          Cancel
+        </Button>
+        <Button
+          color={danger ? 'error' : 'primary'}
+          variant="contained"
+          disabled={isSubmitting}
+          onClick={() => {
+            confirm();
+            if (!closeOnConfirm) closeDialog();
+          }}
+          startIcon={
+            isSubmitting && (
+              <CircularProgress
+                color={danger ? 'error' : 'primary'}
+                size={18}
+              />
+            )
+          }
+        >
+          {confirmButton}
+        </Button>
+      </DialogActions>
+    </Dialog>
+  );
+}
