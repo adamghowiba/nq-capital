41020c379212040cc2d2f2bf22c0edd852eaf79b
Lorrain Tchakoumi <lorraintchakoumi@gmail.com>
2024-04-13 15:19:20 +0100

    chore(app): #12 extended step header to customize rowGap

--- apps/investors-portal-web/components/onboarding/stepHeader.tsx
+++ apps/investors-portal-web/components/onboarding/stepHeader.tsx
@@ -3,10 +3,15 @@ import { Box, Typography } from '@mui/material';
 export interface StepHeaderProps {
   title: string;
   subtitle: string;
+  gap?: number;
 }
-export default function StepHeader({ title, subtitle }: StepHeaderProps) {
+export default function StepHeader({
+  title,
+  subtitle,
+  gap = 2,
+}: StepHeaderProps) {
   return (
-    <Box sx={{ display: 'grid', rowGap: 2 }}>
+    <Box sx={{ display: 'grid', rowGap: 1 }}>
       <Typography variant="h2" color="#202020" fontWeight={500}>
         {title}
       </Typography>
