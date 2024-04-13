f9bcc9f8cd2d3f77e89accecda2772b1ee98a071
Lorrain Tchakoumi <lorraintchakoumi@gmail.com>
2024-04-13 14:52:52 +0100

    chore(app): #12 changed type

--- apps/investors-portal-web/components/utils/OneTextField.tsx
+++ apps/investors-portal-web/components/utils/OneTextField.tsx
@@ -1,15 +1,13 @@
 import { Box, TextField, TextFieldProps, Typography } from '@mui/material';
+import { ReactNode } from 'react';
 
 interface OneTextFieldProps {
-  OneLabel: string;
+  OneLabel: ReactNode;
 }
 
 export type TextFieldPropsExtended = TextFieldProps & OneTextFieldProps;
 
-export function OneTextField({
-  OneLabel,
-  ...props
-}: TextFieldPropsExtended) {
+export function OneTextField({ OneLabel, ...props }: TextFieldPropsExtended) {
   return (
     <Box sx={{ display: 'grid', rowGap: 1 }}>
       <Typography>{OneLabel}</Typography>
