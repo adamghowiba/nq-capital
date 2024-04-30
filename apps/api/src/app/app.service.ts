8306f1d2a4655fda69874ac1a06d7867f147f247
Adam <adamware99@hotmail.com>
2024-04-30 08:57:48 +0400

    ci: trigger

--- apps/api/src/app/app.service.ts
+++ apps/api/src/app/app.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getData(): { message: string } {
-    return ({ message: 'Hello API' });
+    return ({ message: 'API v0.0.1 healthy' });
   }
 }
